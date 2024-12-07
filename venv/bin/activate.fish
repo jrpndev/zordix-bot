00bb1d75c3e7a2ad545bdf933373390b904c9bcb
zmjkk <jrpn.env@gmail.com>
2024-12-07 16:05:31 +0000

    feat : adding new template

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/fernandacodes/zordix-bot/venv
+set -gx VIRTUAL_ENV /home/jose/zordix-bot/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
